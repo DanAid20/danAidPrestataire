6af48aa528caa280fd701133af5fccf4bd52f1c1
stephane2018 <bitebstephane1996@gmail.com>
2021-04-09 18:02:09 +0100

    doctore tabs interfaces
    handleing the page of doctor tabs view

--- lib/views/adhrent_views/hello_screen.dart
+++ lib/views/adhrent_views/hello_screen.dart
@@ -6,7 +6,8 @@ import 'package:danaid/helpers/colors.dart';
 import 'package:danaid/helpers/constants.dart';
 import 'package:danaid/views/adhrent_views/my_coverage_tab.dart';
 import 'package:danaid/views/adhrent_views/my_doctor_tab.dart';
-import 'package:danaid/views/doctor_views/tabs_doctor_views/profil_doctor_view.dart';
+import 'package:danaid/views/doctor_views/tabs_doctor_views/doctor_patient_view.dart';
+import 'package:danaid/views/doctor_views/tabs_doctor_views/rendez_vous_doctor.dart';
 import 'package:danaid/widgets/user_avatar_coverage.dart';
 import 'package:danaid/views/adhrent_views/my_welcome_screen.dart';
 import 'package:flutter/material.dart';
@@ -15,34 +16,61 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:provider/provider.dart';
 
+import '../doctor_views/tabs_doctor_views/home_doctor_view.dart';
+
 class HelloScreen extends StatefulWidget {
   @override
   _HelloScreenState createState() => _HelloScreenState();
 }
 
-class _HelloScreenState extends State<HelloScreen> with SingleTickerProviderStateMixin {
+class _HelloScreenState extends State<HelloScreen>
+    with SingleTickerProviderStateMixin {
   TabController _tabController;
   List tabs = <Widget>[
-    const Tab(text: "Bienvenue",),
-    const Tab(text: "Ma Couverture",),
-    const Tab(text: "Mon Docteur",),
+    const Tab(
+      text: "Bienvenue",
+    ),
+    const Tab(
+      text: "Ma Couverture",
+    ),
+    const Tab(
+      text: "Mon Docteur",
+    ),
   ];
   List tabsDoctor = <Widget>[
-    Tab(child: Row(children: [
-      SvgPicture.asset('assets/icons/Bulk/Hands.svg'),
-      SizedBox(width: wv*2,),
-      Text("Bienvenue")
-    ],),),
-    Tab(child: Row(children: [
-      SvgPicture.asset('assets/icons/Bulk/StethoscopeMini.svg'),
-      SizedBox(width: wv*2,),
-      Text("Mes Services")
-    ],),),
-    Tab(child: Row(children: [
-      SvgPicture.asset('assets/icons/Bulk/Vector.svg'),
-      SizedBox(width: wv*2,),
-      Text("Mes Rendez-vous")
-    ],),)
+    Tab(
+      child: Row(
+        children: [
+          SvgPicture.asset('assets/icons/Bulk/Hands.svg'),
+          SizedBox(
+            width: wv * 2,
+          ),
+          Text("Bienvenue")
+        ],
+      ),
+    ),
+    Tab(
+      child: Row(
+        children: [
+          SvgPicture.asset('assets/icons/Bulk/StethoscopeMini.svg'),
+          SizedBox(
+            width: wv * 2,
+          ),
+          Text("Mes Services")
+        ],
+      ),
+    ),
+    Tab(
+      child: Row(
+        children: [
+          SvgPicture.asset('assets/icons/Bulk/Vector.svg'),
+          SizedBox(
+            width: wv * 2,
+          ),
+          Text("Mes Rendez-vous")
+        ],
+      ),
+    )
   ];
   @override
   void initState() {
@@ -52,94 +80,121 @@ class _HelloScreenState extends State<HelloScreen> with SingleTickerProviderStat
 
   @override
   Widget build(BuildContext context) {
-    UserProvider userProvider = Provider.of<UserProvider>(context, listen: false);
+    UserProvider userProvider =
+        Provider.of<UserProvider>(context, listen: false);
     //AdherentProvider adherentProvider = Provider.of<AdherentProvider>(context, listen: false);
     return SafeArea(
       child: Scaffold(
-        body: NestedScrollView(floatHeaderSlivers: true, 
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
-            return <Widget>[
-              SliverAppBar(
-                toolbarHeight: hv*12,
-                automaticallyImplyLeading: false,
-                backgroundColor: Colors.white,
-                title: Column(
-                  children: [
-                    UserAvatarAndCoverage(),
-                  ],
-                ),
-                actions: [
-                  Stack(
-                    children: [
-                      SizedBox(width: wv*30),
-                      Positioned(
-                        right: 0,
-                        child: InkWell(
-                          onTap: (){},
-                          child: Container(
-                            padding: EdgeInsets.all(wv*3),
-                            child: SvgPicture.asset("assets/icons/Two-tone/Notification.svg", width: wv*7,)
+          body: NestedScrollView(
+              floatHeaderSlivers: true,
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) {
+                return <Widget>[
+                  SliverAppBar(
+                    toolbarHeight: hv * 12,
+                    automaticallyImplyLeading: false,
+                    backgroundColor: Colors.white,
+                    title: Column(
+                      children: [
+                        UserAvatarAndCoverage(),
+                      ],
+                    ),
+                    actions: [
+                      Stack(
+                        children: [
+                          SizedBox(width: wv * 30),
+                          Positioned(
+                            right: 0,
+                            child: InkWell(
+                              onTap: () {},
+                              child: Container(
+                                  padding: EdgeInsets.all(wv * 3),
+                                  child: SvgPicture.asset(
+                                    "assets/icons/Two-tone/Notification.svg",
+                                    width: wv * 7,
+                                  )),
+                            ),
                           ),
-                        ),
-                      ),
-                      
-                      Positioned(
-                        right:wv*1,
-                        top: hv*1,
-                        child: Container(
-                          padding: EdgeInsets.all(3),
-                          decoration: BoxDecoration(
-                            color: Colors.yellow,
-                            borderRadius: BorderRadius.circular(100)
+                          Positioned(
+                            right: wv * 1,
+                            top: hv * 1,
+                            child: Container(
+                              padding: EdgeInsets.all(3),
+                              decoration: BoxDecoration(
+                                  color: Colors.yellow,
+                                  borderRadius: BorderRadius.circular(100)),
+                              child: Text(
+                                "9+",
+                                style: TextStyle(
+                                    fontSize: wv * 2.2,
+                                    color: Colors.teal,
+                                    fontWeight: FontWeight.w900),
+                              ),
+                            ),
                           ),
-                          child: Text("9+", style: TextStyle(fontSize: wv*2.2, color: Colors.teal, fontWeight: FontWeight.w900),),
-                        ),
+                          Positioned(
+                            right: wv * 1,
+                            top: hv * 8,
+                            child: Container(
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "12 000 Pts",
+                                    style: TextStyle(
+                                        fontSize: inch * 1.3,
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.teal[400]),
+                                  ),
+                                  SizedBox(
+                                    width: wv * 2,
+                                  ),
+                                  SvgPicture.asset(
+                                    "assets/icons/Bulk/Shield Done.svg",
+                                    width: 18,
+                                  ),
+                                  SvgPicture.asset(
+                                    "assets/icons/Bulk/Ticket Star.svg",
+                                    width: 18,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-
-                      Positioned(
-                        right: wv*1,
-                        top: hv*8,
-                        child: Container(
-                          child: Row(
-                            children: [
-                              Text("12 000 Pts", style: TextStyle(fontSize: inch*1.3, fontWeight: FontWeight.w700, color: Colors.teal[400]),),
-                              SizedBox(width: wv*2,),
-                              SvgPicture.asset("assets/icons/Bulk/Shield Done.svg", width: 18,),
-                              SvgPicture.asset("assets/icons/Bulk/Ticket Star.svg", width: 18,),
-                            ],
-                         ),
-                        ),
-                      ),
-
                     ],
-                  ),
+                    pinned: true,
+                    floating: true,
+                    bottom: TabBar(
+                        indicatorWeight: 3,
+                        indicatorColor: kPrimaryColor,
+                        isScrollable: true,
+                        controller: _tabController,
+                        labelColor: kPrimaryColor,
+                        labelStyle: TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: inch * 1.7),
+                        unselectedLabelStyle:
+                            TextStyle(fontWeight: FontWeight.w400),
+                        tabs: userProvider.getProfileType == doctor
+                            ? tabsDoctor
+                            : tabs),
+                  )
+                ];
+              },
+              body: TabBarView(
+                controller: _tabController,
+                children: <Widget>[
+                  userProvider.getProfileType == doctor
+                      ? HomeDoctorView()
+                      : MyWelcomeScreen(),
+                  userProvider.getProfileType == doctor
+                      ? DoctorPatientView()
+                      : MyCoverageTabView(),
+                  userProvider.getProfileType == doctor
+                      ? RendezVousDoctorView()
+                      : MyDoctorTabView()
                 ],
-                pinned: true,
-                floating: true,
-                bottom: TabBar(
-                  indicatorWeight: 3,
-                  indicatorColor: kPrimaryColor,
-                  isScrollable: true,
-                  controller: _tabController,
-                  labelColor: kPrimaryColor,
-                  labelStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: inch*1.7),
-                  unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w400),
-                  tabs: userProvider.getProfileType == doctor ? tabsDoctor : tabs
-                ),
-              )
-            ];
-          }, 
-        
-          body: TabBarView(
-            controller: _tabController,
-            children: <Widget>[
-              userProvider.getProfileType == doctor ? ProfilDoctorView() : MyWelcomeScreen(),
-              userProvider.getProfileType == doctor ? ProfilDoctorView() : MyCoverageTabView(),
-              userProvider.getProfileType == doctor ? ProfilDoctorView() : MyDoctorTabView()
-            ],)
-          
-      )
-      ),
+              ))),
     );
   }
 }
--- lib/views/doctor_views/tabs_doctor_views/doctor_patient_view.dart
+++ lib/views/doctor_views/tabs_doctor_views/doctor_patient_view.dart
@@ -1,4 +1,9 @@
+import 'package:danaid/core/utils/config_size.dart';
+import 'package:danaid/helpers/colors.dart';
+import 'package:danaid/helpers/constants.dart';
+import 'package:danaid/widgets/home_page_mini_components.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class DoctorPatientView extends StatefulWidget {
   DoctorPatientView({Key key}) : super(key: key);
@@ -8,8 +13,284 @@ class DoctorPatientView extends StatefulWidget {
 }
 
 class _DoctorPatientViewState extends State<DoctorPatientView> {
+  Widget servicesList() {
+    return Container(
+      margin: EdgeInsets.only(top: inch * 1.5, bottom: inch * 1.5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Column(
+        children: [
+          Container(
+              margin: EdgeInsets.only(
+                  left: inch * 1.5, right: inch * 1.5, top: inch * 3),
+              width: wv * 90,
+              height: hv * 18,
+              decoration: BoxDecoration(
+                color: kThirdIntroColor,
+                boxShadow: [
+                  BoxShadow(
+                      color: kThirdColor, spreadRadius: 0.5, blurRadius: 4),
+                ],
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              padding: EdgeInsets.only(top: hv * 1),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      margin:
+                          EdgeInsets.only(right: inch * 3.5, top: inch * 0.5),
+                      child: SvgPicture.asset(
+                        'assets/icons/Bulk/Bookmark.svg',
+                        width: wv * 8,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                        left: inch * 2.9, right: inch * 1.5, top: inch * 1),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Text(
+                            'Demarer Une consultation',
+                            style: TextStyle(
+                                color: kCardTextColor,
+                                fontWeight: FontWeight.w800,
+                                fontSize: fontSize(size: 20)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                        left: inch * 0.2, right: inch * 1.5, top: inch * 2.5),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Text(
+                            'Accédez au Carnet de Santé digital de vos patients et déclenchez leur prise en charge',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: kCardTextColor,
+                                fontWeight: FontWeight.w500,
+                                fontSize: fontSize(size: 19)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 2.0),
+            height: hv * 15,
+            child: new ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                displsOtherServices(
+                    iconesUrl: 'assets/icons/Bulk/Add User.svg',
+                    title: 'Ajouter un Patient'),
+                displsOtherServices(
+                    iconesUrl: 'assets/icons/Bulk/Chart.svg',
+                    title: 'Ajouter un Patient'),
+                displsOtherServices(
+                    iconesUrl: 'assets/icons/Bulk/Message.svg',
+                    title: 'Ajouter un Patient'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  displsOtherServices({String iconesUrl, String title}) {
+    return Column(
+      children: [
+        Container(
+            margin: EdgeInsets.only(
+                left: inch * 2, right: inch * 1.5, top: inch * 2),
+            width: wv * 24,
+            height: hv * 10,
+            decoration: BoxDecoration(
+              color: kThirdIntroColor,
+              boxShadow: [
+                BoxShadow(color: kThirdColor, spreadRadius: 0.5, blurRadius: 4),
+              ],
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            padding: EdgeInsets.only(top: hv * 0.3),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    margin: EdgeInsets.only(left: inch * 1.5, top: inch * 0.5),
+                    child: SvgPicture.asset(
+                      iconesUrl != null
+                          ? iconesUrl
+                          : 'assets/icons/Bulk/Bookmark.svg',
+                      width: wv * 6,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                      left: inch * 1.9, right: inch * 1.5, top: inch * 1),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: wv * 15,
+                        child: Text(
+                          title != null ? title : 'Ajouter un Patient',
+                          style: TextStyle(
+                              color: kCardTextColor,
+                              fontWeight: FontWeight.w800,
+                              fontSize: fontSize(size: 15)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )),
+      ],
+    );
+  }
+
+  patientOfTodyaList() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Column(children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          padding: EdgeInsets.only(top: hv * 2, left: wv * 5, right: wv * 5),
+          child: Row(
+            children: [
+              Text(
+                "Question au Docteur",
+                style: TextStyle(
+                    color: kPrimaryColor, fontWeight: FontWeight.w700),
+              ),
+              Text("Voir plus..")
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 2.0),
+          color: Colors.white,
+          child: new ListView(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            children: <Widget>[
+              patientsItem(
+                  apointementDate: "12:15",
+                  apointementType: 'RDV',
+                  imgUrl: 'assets/images/avatar-profile.jpg',
+                  nom: 'Fabrice Mbanga',
+                  subtitle: 'Nouvelle Consultation'),
+              patientsItem(
+                  apointementDate: "12:05",
+                  apointementType: 'RDV',
+                  imgUrl: 'assets/images/avatar-profile.jpg',
+                  nom: 'Fabrice Mbanga',
+                  subtitle: 'Nouvelle Consultation',
+                  isSpontane: true),
+              patientsItem(
+                  apointementDate: "12:15",
+                  apointementType: 'RDV',
+                  imgUrl: 'assets/images/avatar-profile.jpg',
+                  nom: 'Fabrice Mbanga',
+                  subtitle: 'Nouvelle Consultation'),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget patientsItem(
+      {String imgUrl,
+      String nom,
+      String subtitle,
+      String apointementDate,
+      String apointementType,
+      bool isSpontane = false}) {
+    return ListTile(
+      leading: HomePageComponents().getAvatar(
+          imgUrl: imgUrl != null ? imgUrl : "assets/images/avatar-profile.jpg",
+          size: wv * 8,
+          renoveIsConnectedButton: false),
+      title: Text(
+        nom != null ? nom : 'Fabrice Mbanga',
+        style: TextStyle(
+            fontSize: fontSize(size: 16),
+            fontWeight: FontWeight.w700,
+            color: kPrimaryColor),
+      ),
+      subtitle: Text(
+        subtitle != null ? subtitle : 'Nouvelle Consultation',
+        style: TextStyle(
+            fontSize: fontSize(size: 14),
+            fontWeight: FontWeight.w500,
+            color: kPrimaryColor),
+      ),
+      trailing: isSpontane == true
+          ? Text(
+              'Spontane',
+              style: TextStyle(fontSize: fontSize(size: 14), color: kDeepTeal),
+            )
+          : Container(
+              padding: EdgeInsets.only(top: hv * 1, right: wv * 2),
+              child: Column(
+                children: [
+                  Text(
+                    apointementDate != null ? apointementDate : '12:15',
+                    style: TextStyle(
+                        fontSize: fontSize(size: 14), color: kPrimaryColor),
+                  ),
+                  Text(
+                    apointementType != null ? apointementType : 'RDV',
+                    style: TextStyle(
+                        fontSize: fontSize(size: 14),
+                        fontWeight: FontWeight.w700,
+                        color: primaryColor),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      physics: NeverScrollableScrollPhysics(),
+      child: Container(
+        decoration: BoxDecoration(
+          color: kBgTextColor,
+        ),
+        child: Column(
+          children: [servicesList(), patientOfTodyaList()],
+        ),
+      ),
+    );
   }
 }
